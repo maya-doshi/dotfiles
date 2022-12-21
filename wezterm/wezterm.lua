b5a3e222b4f1fcd6d79063efd8bfbb7ea11e6cc4
Maya Doshi <mayas@duck.com>
2022-12-20 20:53:19 -0500

    Wezterm Config

---
+++ wezterm/wezterm.lua
@@ -0,0 +1,25 @@
+local wezterm = require 'wezterm';
+
+local custom = wezterm.color.get_builtin_schemes()['Catppuccin Mocha']
+custom.background = '#000000'
+
+return {
+  font = wezterm.font_with_fallback({
+    {
+      family='JetBrainsMono Nerd Font',
+    },
+    'SauceCodePro Nerd Font Mono',
+  }),
+  font_size = 12,
+  enable_tab_bar = false,
+  skip_close_confirmation_for_processes_named = {
+    'bash',
+    'fish',
+  },
+  default_cursor_style = 'BlinkingBar',
+  ssh_backend = 'Ssh2',
+  color_schemes = {
+    ['OLEDppuccin'] = custom,
+  },
+  color_scheme = 'OLEDppuccin',
+}
