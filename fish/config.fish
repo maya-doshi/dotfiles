8c8ff457d7768979a2d47192ab86db13638625c9
Maya Doshi <mayas@duck.com>
2022-12-20 20:56:50 -0500

    fish update

--- fish/config.fish
+++ fish/config.fish
@@ -6,11 +6,22 @@ end
 set -Ux MOZ_ENABLE_WAYLAND 1
 set -Ux MOZ_USE_XINPUT2 1
 set -Ux GDK_BACKEND wayland
+set -Ux AMD_VULKAN_ICD RADV
+set -Ux DEVKITPRO /opt/devkitpro/
+set -Ux DEVKITARM /opt/devkitpro/devkitARM
+set -Ux DEVKITPPC /opt/devkitpro/devkitPPC
 
 # Set up Zoxide [cd alternative]
 zoxide init fish | source
 
 # ALIASES
 alias ls="lsd"
+alias lsa="lsd -a"
 alias tree="lsd --tree"
 alias hx="helix"
+alias alacsh="TERM=xterm-256color ssh"
+alias gap="git add -p"
+alias gaa="git add ."
+alias gic="git commit"
+alias gip="git push"
+alias za="zathura"
