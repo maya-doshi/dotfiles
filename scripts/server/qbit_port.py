ae8c7c6ec690624ed7765893bee0f533008c2870
Maya Doshi <maya-doshi@proton.me>
2023-12-22 07:35:51 -0500

    cleanup

--- scripts/server/qbit_port.py
+++ scripts/server/qbit_port.py
@@ -12,15 +12,14 @@ conn_info = dict(
 )
 
 while (True):
-    cur_port = open(f"{port_path}", "r")
-    cur_port = cur_port.read()
-    cur_port = cur_port.replace("\n", "")
-
-    print(f"File {cur_port}")
     qbt_client = qbittorrentapi.Client(**conn_info)
     try:
+        cur_port = open(port_path, "r").read().replace("\n", "")
+        print(f"File {cur_port}")
+
         qbt_client.auth_log_in()
         print(f"qbit {qbt_client.application.preferences.listen_port}")
+
         if (f"{cur_port}" != f"{qbt_client.application.preferences.listen_port}"):
             prefs = qbt_client.application.preferences
             prefs["listen_port"] = cur_port
